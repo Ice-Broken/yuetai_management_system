051819a7ff3b41c50486d7a3ad4739249b7477e3
Ice-Broken <qingzhizha610@gmail.com>
2023-08-30 16:41:38 +0800

    add char of responsor which is forget

--- backend/buyer/create-buyer.js
+++ backend/buyer/create-buyer.js
@@ -6,7 +6,7 @@ const mysql = require('../mysql')
 
 router.post('/create-buyer', (req, res) => {
     let sql = `INSERT INTO prod_buyer (company_name_ch, company_name_en, subject, source,
-        contact_name, position, tel, email, remark)
+        contact_name, position, tel, email, name, remark)
         VALUES ('${req.body.company_name_ch}', 
                 '${req.body.company_name_en}', 
                 '${req.body.subject}',
@@ -15,6 +15,7 @@ router.post('/create-buyer', (req, res) => {
                 '${req.body.position}',
                 '${req.body.tel}',
                 '${req.body.email}',
+                '${req.body.name}',
                 '${req.body.remark}');`            
     mysql.query(sql, function (err, result) {
         res.json({
